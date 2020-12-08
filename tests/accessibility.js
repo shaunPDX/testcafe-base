2e579400d7b498c9b980c3765e759d867612c26b
Shaun <shaun.foran@gmail.com>
2020-12-08 14:33:15 -0800

    Adding Node GitHub workflow

--- tests/accessibility.js
+++
@@ -1,9 +0,0 @@
-import { axeCheck, createReport } from 'axe-testcafe';
- 
-fixture `TestCafe tests with Axe`
-    .page `http://example.com`;
- 
-test('Automated accessibility testing', async t => {
-    const { error, violations } = await axeCheck(t);
-    await t.expect(violations.length === 0).ok(createReport(violations));
-});
